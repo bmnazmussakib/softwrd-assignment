ba54faf68e58e1ee3b8717de573a947b42e70516
bmnazmussakib <nazmussakib161@gmail.com>
2023-09-02 16:13:56 +0600

    country data loaded

---
+++ src/routes/chart/+page.js
@@ -0,0 +1,11 @@
+export const load = async (loadEvent) => {
+  const { fetch } = loadEvent;
+  const uri = "https://restcountries.com/v3.1/all";
+  const res = await fetch(uri);
+  const allCountry = await res.json();
+
+  // get only first 10 data from an api
+  const firstTenCountry = allCountry.slice(0,9)
+
+  return {firstTenCountry};
+};
---
+++ src/routes/chart/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  // your script goes here
+  export let data;
+  console.log(data)
+</script>
+
+<h1 class="text-3xl font-bold underline text-blue-700">Chart</h1>
+
+<style>
+  /* your styles go here */
+</style>
