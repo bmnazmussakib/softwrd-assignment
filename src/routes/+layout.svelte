ce261c722776474039b4410b1ca83cc40eb6a93a
bmnazmussakib <nazmussakib161@gmail.com>
2023-09-03 23:42:56 +0600

    svelte-chartjs uninstaller

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
   import "../app.css";
 </script>
 
+<header>
+  <ul class="list-none m-0 p-0 overflow-hidden bg-slate-500 flex">
+    <li><a class="text-white text-center no-underline px-6 py-3.5 block uppercase font-medium hover:bg-slate-800" href="/">Home</a></li>
+    <li><a class="text-white text-center no-underline px-6 py-3.5 block uppercase font-medium hover:bg-slate-800" href="/layout">Layout</a></li>
+    <li><a class="text-white text-center no-underline px-6 py-3.5 block uppercase font-medium hover:bg-slate-800" href="/chart">Chart</a></li>
+    <li><a class="text-white text-center no-underline px-6 py-3.5 block uppercase font-medium hover:bg-slate-800" href="/map">Map</a></li>
+  </ul>
+</header>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
-<h1 class="text-3xl font-bold underline text-red-700">
-    Hello world!
-  </h1>
+<div class="flex justify-center items-center h-screen">
+  <div>
+    <h1 class="text-3xl font-medium mb-5 text-blue-500">Softwrd Assignment</h1>
+    <ul>
+      <li class="text-xl"><span class="font-medium">Name:</span> B. M. Nazmus Sakib</li>
+      <li class="text-xl"><span class="font-medium">Github:</span> <a href="https://github.com/bmnazmussakib/softwrd-assignment">Click Here</a></li>
+    </ul>
+  </div>
+</div>
   
   <style lang="postcss">
     :global(html) {
